65c92e8f60de4bd943505ed38a055926906ced70
Sylwinn Tudor <sylwinntudor@Sylwinns-MacBook-Air.local>
2017-09-12 22:16:15 -0500

    added var to students and courses with console logs

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ var avgRating = (rating1+rating2+rating3)/3;
 avgRating = avgRating.toFixed(2);
 
 console.log("Teacher: " + teacherName + "\nDepartment: " + department + "\nRatings: " + rating1 +", " +rating2 +", " + rating3 + "\nAvg. Rating: " +avgRating);
+
+// Students:
+var studentName = "Punkk Y. Gregorson";
+var major ="Painting";
+var gpa =2.8;
+console.log("Name: " +studentName + "\nMajor: " +major +"\nG.P.A: " +gpa);
+
+//Courses:
+var courseName ="Robotics";
+var semester ="Fall 2017";
+console.log("Course: " + courseName +"\nTeacher: " +teacherName +"\nSemester: " +semester);
