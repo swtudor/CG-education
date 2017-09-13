ace069095b0755cdcddba8fcfb2619fc667226e2
Sylwinn Tudor <sylwinntudor@Sylwinns-MacBook-Air.local>
2017-09-12 22:00:01 -0500

    First foray into JS. var added on teachers.html. Kinda

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("This file is linked!");
+var teacherName = "Zak VonBolt";
+var department = "Engineering";
+var rating1 = 3.2;
+var rating2 = 4.5;
+var rating3 = 3.8;
+var avgRating = (rating1+rating2+rating3)/3;
+avgRating = avgRating.toFixed(2);
+
+console.log("Teacher: " + teacherName + "\nDepartment: " + department + "\nRatings: " + rating1 +", " +rating2 +", " + rating3 + "\nAvg. Rating: " +avgRating);
