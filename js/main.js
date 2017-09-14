7fc4711c982f6c3f480d62b72c68d29d309d5d1b
Sylwinn Tudor <sylwinntudor@Sylwinns-MacBook-Air.local>
2017-09-13 21:36:39 -0500

    updated js to reflect corrected formulas

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 console.log("This file is linked!");
 var teacherName = "Zak VonBolt";
 var department = "Engineering";
-var rating1 = 3.2;
-var rating2 = 4.5;
-var rating3 = 3.8;
-var avgRating = (rating1+rating2+rating3)/3;
+var ratingOne = 3.2;
+var ratingTwo = 4.5;
+var ratingThree = 3.8;
+var avgRating = (ratingOne+ratingTwo+ratingThree)/3;
 avgRating = avgRating.toFixed(2);
 
-console.log("Teacher: " + teacherName + "\nDepartment: " + department + "\nRatings: " + rating1 +", " +rating2 +", " + rating3 + "\nAvg. Rating: " +avgRating);
+console.log("Teacher: " + teacherName + "\nDepartment: " + department + "\nRatings: " + ratingOne +", " +ratingTwo +", " + ratingThree + "\nAvg. Rating: " +avgRating);
 
 // Students:
 var studentName = "Punkk Y. Gregorson";
