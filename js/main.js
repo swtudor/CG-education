aa38f5665c895aa34c3472a37c20b6f22f1145d5
Sylwinn Tudor <sylwinntudor@Sylwinns-MacBook-Air.local>
2017-09-26 14:28:53 -0500

    The actual corrected function getAvgRating with a return and everything.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-console.log("This file is linked!");
 // Teachers:
 var teacherName = "Zak VonBolt";
 var department = "Engineering";
 var rating =[3.8, 4.0, 3.2];
-console.log (rating)
 
-function getAvgRating (){
+function getAvgRating (rating){
   var total = 0;
   for(var i = 0; i < rating.length; i++) {
     total += rating[i];
   }
-  avg = total / rating.length;
+  var avg = total / rating.length;
   avg = avg.toFixed(2);
-  console.log (avg);
+  return avg;
 }
 var avgRating = getAvgRating (rating);
 
-console.log("Teacher: " + teacherName + "\nDepartment: " + department + "\nRatings: " + rating + "\nAvg. Rating: " +avg);
+console.log("Teacher: " + teacherName + "\nDepartment: " + department + "\nRatings: " + rating + "\nAvg. Rating: " +avgRating);
 
 // Students:
 var studentName = "Punkk Y. Gregorson";
