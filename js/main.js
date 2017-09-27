ddc07ce65f7cb1e8ac146f46dd32f034974be60b
Sylwinn Tudor <sylwinntudor@Sylwinns-MacBook-Air.local>
2017-09-27 14:03:04 -0500

    addTeacherRating functional though not ideal.

--- js/main.js
+++ js/main.js
@@ -2,19 +2,49 @@
 var teacherName = "Zak VonBolt";
 var department = "Engineering";
 var rating =[3.8, 4.0, 3.2];
+var newRating = 2.5;
 
 function getAvgRating (rating){
   var total = 0;
-  for(var i = 0; i < rating.length; i++) {
+  for(var i = 0; i <rating.length; i++) {
     total += rating[i];
   }
   var avg = total / rating.length;
   avg = avg.toFixed(2);
   return avg;
 }
+
+function addTeacherRating(newRating, rating){
+  rating.push(newRating);
+  return rating;
+}
+
+rating = addTeacherRating (newRating, rating);
+console.log (rating);
+
 var avgRating = getAvgRating (rating);
 
+
 console.log("Teacher: " + teacherName + "\nDepartment: " + department + "\nRatings: " + rating + "\nAvg. Rating: " +avgRating);
+
+/* seems like a better choice for the addTeacherRating function but it keeps going wonky. Want to fix
+
+function addTeacherRating(newRating, rating){
+  if (rating.length==0){
+    rating.push(newRating);
+  }else{
+    for (var i=0; i<rating; i++){
+      if(rating [i]==undefined){
+        rating[i] = newRating;
+        return rating;
+      }else if (i==rating.length -1){
+        rating.push(newRating);
+        return rating;
+      }
+    }
+  }
+}
+*/
 
 // Students:
 var studentName = "Punkk Y. Gregorson";
