d4fd7d6ae6d1b888210f470c5bbc05b0f61c1e83
Sylwinn Tudor <sylwinntudor@Sylwinns-MacBook-Air.local>
2017-10-06 09:42:57 -0500

    Fiexed the for-loop in the double array. Woohoo

--- js/main.js
+++ js/main.js
@@ -20,10 +20,10 @@ function getAvgRating (rating){
 }
 
 // Instructions-
+/* !!!!Commented out temprorarily!!!!
 var newRating = parseFloat(prompt("We would like for you to review you instructor. Please enter a rating between 0.0 - 5.0?"));
-
 if(newRating >5.0){
-  parseFloat(prompt("Please re-enter a variable that is between 0.0 and 5.0"));
+  parseFloat(confirm("Please re-enter a variable that is between 0.0 and 5.0"));
 }else{
   console.log(teacherRating);
 }
@@ -32,7 +32,7 @@ var avgRating = getAvgRating (teacherRating);
 console.log(teacherRating + avgRating);
 alert("Thanks for you review!" +teacherName +" average rating is now " +avgRating);
 console.log("Teacher: " + teacherName + "\nDepartment: " + department + "\nRatings: " + teacherRating + "\nAvg. Rating: " +avgRating);
-
+*/
 // Students:
 var studentName = "Punkk Y. Gregorson";
 var major ="Painting";
@@ -40,7 +40,48 @@ var gpa =2.8;
 console.log("Name: " +studentName + "\nMajor: " +major +"\nG.P.A: " +gpa);
 
 //Courses:
+// Declarations:
 var courseName ="Robotics";
 var semester ="Fall 2017";
-var courses=[[courseTitle, Department], ]
+var allCourses=[
+  ["Robotics", "Engineering"],
+  ["Mammals: Changes Post-Nuclear War", "Biology"],
+  ["Birds!", "Biology"],
+  ["Blargh!!!", "Engineering"],
+  ["20th Century Baking Techniques", "History"],
+  ["Self-Paving Roads", "Engineering"],
+  ["Raining Poodles", "Meterology"]
+  ["Yippee!","History"]];
+
+//Instructions:
+/* The function should return a new array
+filled with courses that are ONLY in the department specified in the parameter.*/
+
+function searchCourses (coursesArray, departmentSearched){
+  var newArray=[];
+  for (var i=0; i<coursesArray.length;i++){
+      if (coursesArray[i][1]==departmentSearched){
+        newArray.push(coursesArray[i][0]);
+        newArray[i++];
+      }else{
+        console.log("FAIL");
+      }
+  } return newArray;
+}
+
+var test=searchCourses(allCourses, department);
+console.log(test);
+
+/*
+  for(var i=0; i<coursesArray.length; i++){
+    if (coursesArray[i][1]===departmentSearched){
+      return departmentSearched;
+      console.log(departmentSearched);
+    }else{
+      return console.log("There are no courses currently available in "+departmentSearched + " at this point.");
+    }
+  }
+}*/
+//Instructions:
+allCourses=searchCourses(allCourses, department);
 console.log("Course: " + courseName +"\nTeacher: " +teacherName +"\nSemester: " +semester);
