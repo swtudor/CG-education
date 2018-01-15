dc68acaadbe2c3a97241ff1e1eaf696c7010b028
Sylwinn Tudor <sylwinntudor@Sylwinns-MacBook-Air.local>
2018-01-14 18:38:05 -0600

    restructured DOM to dry up student page, wrote updateStudentDisplay to make page dynamic

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ Declarations
 const sum = (a, b) => a + b;
 
 /*****************
-Teacher page- variables & objects constructors:
+Teacher page- variables & constructors:
 *****************/
 function Teacher (name, department, ratings, coursesArray, imgsrc){
   this.name = name;
@@ -34,24 +34,26 @@ Teacher.prototype = {
      }return coursesTaught;
    },
 }
-
-var teacherZak = new Teacher("Zak Masters", "Engineering", [3.8, 4.0, 3.2], ['MATH 1800', 'ENGR 1010'], "./images/zak.jpg");
-var teacher2 = new Teacher ("Jack English", "Meteorology", [5.0, 4.0, 4.5], ["METEO 101", 'METEO 401'], "./images/zippy.jpg");
-var teacher3 = new Teacher ("Martha Blick", "History", [3.8, 4.0, 3.2], ['Modern History', 'HIST:601'], "./images/pinky-puffpuff.jpg");
-var teacher4 = new Teacher ("Jane Reed", "History", [4.7, 3.5, 4.0], ['20th Cent. Russia', 'Hist 101'], "./images/prof-cupcake.jpg");
-var teacher5 = new Teacher ("Steve Botts", "Computer Science", [5.0, 5.0, 5.0], ['Algorithms', 'Robotics 301'], "./images/beep.jpg");
+// teacher object map(name, department, ratings, coursesArray, imgsrc)
+var teacherZak = new Teacher("Zak Masters", "Engineering", [3.8, 4.0, 3.2], ['MATH 1800', 'ENGR 1010'], "https://upload.wikimedia.org/wikipedia/commons/7/7e/Red_Skelton_1960_rebalance.jpg");
+var teacher2 = new Teacher ("Jack English", "Meteorology", [5.0, 4.0, 4.5], ["METEO 101", 'METEO 401'], "https://upload.wikimedia.org/wikipedia/commons/2/27/Grant%2C_Cary_%28Suspicion%29_01_Crisco_edit.jpg");
+var teacher3 = new Teacher ("Martha Blick", "History", [3.8, 4.0, 3.2], ['Modern History', 'HIST:601'], "https://upload.wikimedia.org/wikipedia/commons/2/26/Gloria_Stuart_ca._1930s_Twentieth_Century_Fox_headshot.png");
+var teacher4 = new Teacher ("Jane Reed", "History", [4.7, 3.5, 4.0], ['20th Cent. Russia', 'Hist 101'], "https://upload.wikimedia.org/wikipedia/commons/8/8f/Olivia_de_Havilland_Publicity_Photo_1952.jpg");
+var teacher5 = new Teacher ("Steve Botts", "Computer Science", [5.0, 5.0, 5.0], ['Algorithms', 'Robotics 301'], "https://upload.wikimedia.org/wikipedia/commons/a/a4/James_cagney_promo_photo.jpg");
 
 var allTeachers=[teacherZak, teacher2, teacher3, teacher4, teacher5];
 
 /******************
-Student Page variables & object constructors:
+Student Page variables & constructors:
 ******************/
-function Student (name, major, email, gpa, coursesArray){
+function Student (name, major, email, gpa, coursesArray, imgsrc){
   this.name = name;
   this.major = major;
   this.email = email;
   this.gpa = gpa;
   this.courses = [coursesArray];
+  this.img = new Image ();
+  this.img.src = imgsrc;
 }
 Student.prototype = {
   addCourse: function (course){
@@ -69,14 +71,15 @@ Student.prototype = {
   }
 }
 
-//student object map (name, major, email, gpa, courseArray)
-var student1 = new Student ("Daniel McGregor", "Painting", "email@school.edu", 3.5, ["3D Desgin", "20th Cent Art"]);
-var student2 = new Student ("Grace Regale", "History", "email@school.edu", 4.0, ["Magic in the Middle Ages", "Modern History"]);
-var student3 = new Student ("Owen James", "Mechanical Engineering", 'email@school.edu', 3.75, ["Artificial Intelligence", "Painting 101"]);
-const student4 = new Student ("Iris Edwards", "Engineering", 'email@school.edu', 3.8, ['Geology', 'Civil Engineering']);
-const student5 = new Student ('Elsie Parks', 'Dance', 'email@chool.edu', 3.0, ['Modern Dance', 'Ballet:301']);
-const student6 = new Student ('Elliot Majors', 'Meteorology', 'email@school.edu', 3.5, ['Meteo:201', '3D Design']);
+//student object map (name, major, email, gpa, courseArray, imgsrc)
+var student1 = new Student ("Daniel McGregor", "Painting", "email@school.edu", 3.5, ["3D Desgin", "20th Cent Art"], 'https://upload.wikimedia.org/wikipedia/commons/3/30/Ian-clarke-headshot-1.jpg');
+var student2 = new Student ("Grace Regale", "History", "email@school.edu", 4.0, ["Magic in the Middle Ages", "Modern History"], 'https://upload.wikimedia.org/wikipedia/commons/5/51/Auset_Maat_Kheru.jpg');
+var student3 = new Student ("Owen James", "Mechanical Engineering", 'email@school.edu', 3.75, ["Artificial Intelligence", "Painting 101"], 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Ryan_Driller_2016.jpg');
+const student4 = new Student ("Iris Edwards", "Engineering", 'email@school.edu', 3.8, ['Geology', 'Civil Engineering'], 'https://upload.wikimedia.org/wikipedia/commons/6/6a/12_01_2016_Alice_headshot.jpg');
+const student5 = new Student ('Elsie Parks', 'Dance', 'email@chool.edu', 3.0, ['Modern Dance', 'Ballet:301'], 'https://upload.wikimedia.org/wikipedia/commons/9/93/Chloe_Salmon.jpg');
+const student6 = new Student ('Elliot Majors', 'Meteorology', 'email@school.edu', 3.5, ['Meteo:201', '3D Design'], 'https://upload.wikimedia.org/wikipedia/commons/b/b3/Headshot_2014_2014-02-25_11-45.jpg');
 
+const allStudents = [student1, student2, student3, student4, student5, student6];
 /***************
 Courses Page variables & constructors
 ****************/
@@ -161,6 +164,33 @@ function updateTeacherPanel (teachObj){
   );
 }
 
+/*************
+Student Page functions
+*************/
+
+//student object map (name, major, email, gpa, courseArray, imgsrc)
+function updateStudentDisplay (studentArray){
+  $('#studentContainer').empty();
+  for (var i=0; i<studentArray.length; i++){
+    $('#studentContainer').append(
+      `<div class="student-profile">
+        <box class="student-name">
+          <img class="profile-pic" src="`+ studentArray[i].img.src +`" alt="profile picture"/>
+          <h2> ` + studentArray[i].name +`</h2>
+          <h3>`+studentArray[i].major +`</h3>
+        </box>
+        <box class="bottom-box-student">
+
+            <div><p><span class= 'h3Faker'>Email: </span>  `+ studentArray[i].email +`</p></div>
+            <div><p><span class='h3Faker'>GPA: </span>  `+ studentArray[i].gpa +`</p></div>
+            <div><p><span class= 'h3Faker'>Courses: </span>  `+ studentArray[i].courses[0][0] +`, &ensp;`+ studentArray[i].courses[0][1]+`</p></div>
+        </box>
+      </div>`
+    );
+  };
+}
+
+
 /************
 Course Page Functions:
 *************/
@@ -210,6 +240,7 @@ function searchCourses (coursesArray, departmentSearched){
 $(document).ready(function(){
     updateCourseDisplay(allCourses);
     updateTeacherDisplay(allTeachers);
+    updateStudentDisplay(allStudents);
 
     // search filters/dropdowns on Courses page :
     $('.dropbtn').on('click', function(){
